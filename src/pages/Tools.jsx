9b2de35ba7604ad1069bf751febe25f2ef46250e
Ranju Shrestha <ranjustha8844@gmail.com>
2026-02-27 13:48:16 +0545

    resize logo

--- src/pages/Tools.jsx
+++ src/pages/Tools.jsx
@@ -23,11 +23,11 @@ const Tools = () => {
         {tools.map((tool, index) => (
           <div key={index} className="flex flex-col items-center">
             {/* Circle Image */}
-            <div className="w-28 h-28  lg:w-32 lg:h-32 rounded-full flex items-center justify-center overflow-hidden  hover:scale-105 transition-transform duration-500 ease-in-out">
+            <div className="w-28 h-28  lg:w-30 lg:h-30 flex items-center justify-center overflow-hidden  hover:scale-105 transition-transform duration-500 ease-in-out">
               <img
                 src={tool}
                 alt={titles[index]}
-                className="w-full h-full object-cover rounded-full"
+                className="w-full h-full object-cover "
               />
             </div>
             {/* Title below image */}

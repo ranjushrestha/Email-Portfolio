d6fa5d3fcca463b6b7e048d8efefaa4d34667ec2
Ranju Shrestha <ranjustha8844@gmail.com>
2026-02-27 14:07:13 +0545

    final change

--- src/components/layouts/Footer.jsx
+++ src/components/layouts/Footer.jsx
@@ -1,7 +1,7 @@
 
 const Footer = () => {
   return (
-    <footer className="bg-gray-200 shrink-0 text-gray-500 p-4 text-center mt-auto">
+    <footer className="bg-gray-200 shrink-0 text-gray-500 p-1 text-center mt-auto">
      © 2026 Raju Shrestha - Email Outreach Specialist
      <p className="text-xs text-gray-400">Helping B2B companies scale with data-driven cold email systems</p>
     </footer>

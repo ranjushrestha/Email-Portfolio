a34e13579d2e9d7472a7eb1069ddb6912914cbee
Ranju Shrestha <ranjustha8844@gmail.com>
2026-03-08 19:08:44 +0545

    feat: update components and improve layout

--- src/components/layouts/Sidebar.jsx
+++ src/components/layouts/Sidebar.jsx
@@ -75,7 +75,7 @@ const Sidebar = ({ active }) => {
           />
         </a>
 
-        <h3 className="text-2xl text-gray-100  tracking-wide">RAJU SHRESTHA</h3>
+        <h3 className="text-2xl text-gray-300 font-semibold tracking-wide">RAJU SHRESTHA</h3>
         {/* Hamburger Menu */}
         <button
           onClick={() => setIsOpen(!isOpen)}
